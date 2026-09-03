e05a067f778ba9169311318dd94ef29cf1b4fbb2
Parishukla20 <prernashukla458@gmail.com>
2026-09-03 22:19:32 +0530

    add signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import "./Signup.css";
-
+import img1 from '../assets/IMAGES/f976673a689a5f7b9a657e2a8486dc70.png'
 const Signup = () => {
     return (
-        <div className="form">
-            <form>
-                <h1>Create Account</h1>
+        <div className="signup">
+            <div className="sign-img">
+                <img src={img1} alt="" />
+            </div>
 
-                <label htmlFor="name">Full Name</label>
-                <input type="text" id="name" />
+            <div className="form">
+                <form>
+                    <h1>Welcome! Create Your Account</h1>
 
-                <label htmlFor="email">Enter Mail</label>
-                <input type="email" id="email" />
+                    <label htmlFor="name">Full Name:</label>
+                    <input type="text" id="name" />
 
-                <label htmlFor="phone">Enter Mobile No.</label>
-                <input type="tel" id="phone" />
+                    <label htmlFor="email">Enter Mail:</label>
+                    <input type="email" id="email" />
 
-                <label htmlFor="address">Enter Address</label>
-                <textarea id="address" rows="4"></textarea>
+                    <label htmlFor="phone">Enter Mobile No.:</label>
+                    <input type="tel" id="phone" />
 
-                <label htmlFor="createPass">Create Password</label>
-                <input type="password" id="createPass" />
+                    <label htmlFor="address">Enter Address:</label>
+                    <textarea id="address" rows="4"></textarea>
 
-                <label htmlFor="confirmPass">Confirm Password</label>
-                <input type="password" id="confirmPass" />
+                    <label htmlFor="createPass">Create Password:</label>
+                    <input type="password" id="createPass" />
 
-                <button type="submit">Create Account</button>
+                    <label htmlFor="confirmPass">Confirm Password:</label>
+                    <input type="password" id="confirmPass" />
 
-                <p>
-                    Already have an account? <span>Login Now</span>
-                </p>
-            </form>
+                    <button type="submit">Create Account</button>
+
+                    <p>
+                        Already have an account? <span>Login Now</span>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 };
